976508e71f0137fea6ddf77dfbfd007e6ad16c3d
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-11-13 17:51:57 +0200

    Create application

---
+++ Save Those Princesses/Assets/SaveThosePrincesses/Code/ApplicationLifecycle/ApplicationController.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace SaveThosePrincesses.ApplicationLifecycle
+{
+    public class ApplicationController : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
